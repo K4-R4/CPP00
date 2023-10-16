cde6899605535a103bd8443732e12cf5b80738fd
K4R4 <mabdman512@gmail.com>
2023-10-16 12:34:19 +0900

    Configure idea

--- ex01/include/Contact.hpp
+++ ex01/include/Contact.hpp
@@ -17,21 +17,16 @@
 
 class	Contact
 {
-	private:
-		std::string	first_name;
-		std::string	last_name;
-		std::string	nickname;
-		std::string	phone_number;
-		std::string	darkest_secret;
+private:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
 
-	public:
-		Contact(std::string first_name, std::string last_name, std::string nickname,
-				std::string phone_number, std::string darkest_secret);
-		std::string	get_first_name(void);
-		std::string	get_last_name(void);
-		std::string	get_nickname(void);
-		std::string	get_phone_number(void);
-		std::string	get_darkest_secret(void);
+public:
+	Contact(std::string first_name, std::string last_name, std::string nickname,
+			std::string phone_number, std::string darkest_secret);
 };
 
 #endif
---
+++ ex01/src/Contact.cpp
@@ -0,0 +1,7 @@
+#include <string>
+
+
+int	main(void)
+{
+	Contact c = Contact("a", "i", "u", "e", "o");
+}
